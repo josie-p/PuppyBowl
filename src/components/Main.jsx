67c36c89a593618bc1b961388130aaeba9d8d874
Your Name <parker.josie.eden@gmail.com>
2023-02-16 12:55:53 -0600

    first commit, worked on pulling from api

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Navbar } from "./";
+const BASE = 'https://fsa-puppy-bowl.herokuapp.com/api/2301-FTB-ET-WEB-FT/players';
 
 const Main = () => {
+const [puppies, setPuppies] = useState({});
+
+async function getPuppies(){
+    // const response = await fetch(BASE);
+    // const result = await response.json();
+
+    try{
+        const response = await fetch(`${BASE}`);
+        const result = await response.json();
+        const puppies = result.data
+        console.log(puppies, 'result of fetch!');
+        setPuppies(puppies);
+    }catch(error){
+        console.log(error)
+    }
+    
+}
+
+useEffect(() => {
+    getPuppies();
+    // console.log(puppies.data.players, 'puppy objects');
+}, [])
+
     return(
         <div id="main">
             <Navbar />
